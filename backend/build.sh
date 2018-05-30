026aecbad3c1f9e5eaa19d16e947b78c1cfeab53
dvonthenen <david.vonthenen@emc.com>
2018-05-30 08:34:43 -0700

    Update demo and include tagging

--- backend/build.sh
+++ backend/build.sh
@@ -1,10 +1,15 @@
 #! /usr/bin/env bash
 
-#rm -rf ./vendor
-#rm glide.lock
-#rm ./scaleio-scheduler
-#glide up
+rm -rf ./vendor
+rm glide.lock
+rm ./backend
+glide up
 
-grep -R --exclude-dir vendor --exclude-dir .git --exclude-dir mesos --exclude build.sh TODO ./
+grep -R --exclude-dir vendor --exclude-dir .git --exclude build.sh TODO ./
 
-GOOS=linux GOARCH=amd64 go build .
+#GOOS=linux GOARCH=amd64 go build .
+docker run --rm -e GOPATH=/usr/src/go -v ~/go:/usr/src/go -w /usr/src/go/src/github.com/dvonthenen/jop-stack/backend golang:1.10.2-alpine3.7 go build -v
+
+#docker build -t dvonthenen/jop-backend .
+#docker tag dvonthenen/jop-backend dvonthenen/jop-backend:v0.0.2
+#docker push dvonthenen/jop-backend:v0.0.2
