919796865dd9b066577c1c1b3393e0b8d5f61131
dvonthenen <davidvonthenen@gmail.com>
2018-07-27 10:47:03 -0700

    Update

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./vendor
 rm glide.lock
-rm ./backend
+rm -f ./backend
 glide up
 
 grep -R --exclude-dir vendor --exclude-dir .git --exclude build.sh TODO ./
@@ -10,6 +10,6 @@ grep -R --exclude-dir vendor --exclude-dir .git --exclude build.sh TODO ./
 #GOOS=linux GOARCH=amd64 go build .
 docker run --rm -e GOPATH=/usr/src/go -v ~/go:/usr/src/go -w /usr/src/go/src/github.com/dvonthenen/jop-stack/backend golang:1.10.2-alpine3.7 go build -v
 
-#docker build -t dvonthenen/jop-backend .
-#docker tag dvonthenen/jop-backend dvonthenen/jop-backend:v0.0.2
-#docker push dvonthenen/jop-backend:v0.0.2
+docker build -t dvonthenen/jop-backend .
+docker tag dvonthenen/jop-backend dvonthenen/jop-backend:v0.0.3
+docker push dvonthenen/jop-backend:v0.0.3
